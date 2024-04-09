68b3da3b78ad7e680fc098d195b0c8a694b4026f
Sagespellman <sagespellman@outlook.com>
2024-04-09 11:18:43 -0400

    fixed bug in ensureloggedin file

--- config/ensureLoggedIn.js
+++ config/ensureLoggedIn.js
@@ -1,4 +1,7 @@
-module.exports = fucntion (req, res, next) {
-    if (req.isAuthenticated() ) return next ();
-    res.redirected('/auth/google');
-};
+// Middleware for routes that require a logged in user
+module.exports = function(req, res, next) {
+    // Pass the req/res to the next middleware/route handler
+    if ( req.isAuthenticated() ) return next();
+    // Redirect to login if the user is not already logged in
+    res.redirect('/auth/google');
+  }
