1105685d16fe721e1dead665aa702f1ade8acf72
Sagespellman <sagespellman@outlook.com>
2024-04-09 13:01:36 -0400

    installed depend

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Home', { title: 'Boba Buddies' });
-});
+const passport = require('passport');
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
@@ -28,13 +23,16 @@ router.get('/oauth2callback', passport.authenticate(
   }
 ));
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('Home', { title: 'Boba Buddies' });
+});
+
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function() {
     res.redirect('/');
   });
 });
-
-
 
 module.exports = router;
